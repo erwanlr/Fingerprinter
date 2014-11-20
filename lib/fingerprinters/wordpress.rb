6488ea469b61985fc45abcf40744ed3b2c0465f8
erwanlr <erwan.lr@gmail.com>
2014-11-20 20:12:22 +0100

    Updates WP fingerprints

--- lib/fingerprinters/wordpress.rb
+++ lib/fingerprinters/wordpress.rb
@@ -5,7 +5,7 @@ class Wordpress < Fingerprinter
 
   def downloadable_versions
     versions = {}
-    page     = Nokogiri::HTML(Typhoeus.get('http://wordpress.org/download/release-archive/').body)
+    page     = Nokogiri::HTML(Typhoeus.get('https://wordpress.org/download/release-archive/').body)
 
     page.css('.widefat').first.css('tbody tr td:first').each do |node|
       version = node.text.strip
