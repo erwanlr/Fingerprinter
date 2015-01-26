e8483149794f35f817998930cace7f283acfb484
erwanlr <erwan.lr@gmail.com>
2015-01-26 16:15:26 +0100

    Adds Django-cms fingerprints

---
+++ lib/fingerprinters/django_cms.rb
@@ -0,0 +1,27 @@
+
+# Django CMS
+class DjangoCms < Fingerprinter
+  include Experimental
+  include IgnorePattern::Python
+
+  def downloadable_versions
+    versions = {}
+    page     = Nokogiri::HTML(Typhoeus.get('https://github.com/divio/django-cms/releases').body)
+
+    page.css('h3 a span.tag-name').each do |node|
+      version = node.text.strip
+
+      next unless version =~ /\A[0-9\.]+\z/ # Only stable versions
+
+      versions[version] = "https://github.com/divio/django-cms/archive/#{version}.zip"
+    end
+
+    versions
+  end
+
+  def extract_archive(archive_path, dest)
+    super(archive_path, dest)
+
+    rebase(File.join(dest, 'cms'), dest)
+  end
+end
---
+++ lib/ignore_pattern/python.rb
@@ -0,0 +1,8 @@
+module IgnorePattern
+  # Python
+  module Python
+    def ignore_pattern
+      /\A*.(py|pyc)\z/i
+    end
+  end
+end
