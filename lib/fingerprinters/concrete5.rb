76b49bfab876b1193e8c296fc84472fedca366cd
erwanlr <erwan.lr@gmail.com>
2015-03-02 19:24:05 +0100

    Adds concrete5 cms fingerprints

---
+++ lib/fingerprinters/concrete5.rb
@@ -0,0 +1,28 @@
+
+# Concrete5
+class Concrete5 < Fingerprinter
+  include Experimental
+  include IgnorePattern::PHP
+
+  def downloadable_versions
+    versions = {}
+    page = Nokogiri::HTML(Typhoeus.get('http://www.concrete5.org/developers/developer-downloads/').body)
+
+    page.css('div#body-content p a').each do |node|
+      version = node.parent.text.strip[/\A([0-9\.]+)\s*\(/i, 1]
+      dl_path = node['href'].strip
+
+      versions[version] = "http://www.concrete5.org#{dl_path}"
+    end
+
+    versions
+  end
+
+  protected
+
+  # The Extension is not in the url, so we force it
+  # Otherwise the extraction will fail
+  def archive_extension(_url)
+    '.zip'
+  end
+end
