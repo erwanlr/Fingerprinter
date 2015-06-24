3e3dc45c292443d35524d24e1b547bcf4cb1322f
erwanlr <erwan.lr@gmail.com>
2015-06-24 14:59:27 +0100

    Updates the Umbraco fingerprints - Fixes #10

--- lib/fingerprinters/umbraco.rb
+++ lib/fingerprinters/umbraco.rb
@@ -4,33 +4,15 @@ class Umbraco < Fingerprinter
   include Experimental
   include IgnorePattern::ASP
 
-  def downloadable_versions
-    historical_versions.merge(current_versions)
-  end
-
   def site_url
     'https://our.umbraco.org'
   end
 
-  def current_versions
+  def downloadable_versions
     versions = {}
-    json     = JSON.parse(Typhoeus.get("#{site_url}/api/GetAllFromFile/").body)
+    page     = Nokogiri::HTML(Typhoeus.get("#{site_url}/contribute/releases").body)
 
-    json.each do |obj|
-      if obj['currentRelease']
-        version           = obj['version']
-        versions[version] = download_link("#{site_url}/contribute/releases/#{version.delete('.')}/")
-      end
-    end
-
-    versions
-  end
-
-  def historical_versions
-    versions = {}
-    page     = Nokogiri::HTML(Typhoeus.get("#{site_url}/download").body)
-
-    page.css('div.version h2 a').each do |node|
+    page.css('div h5 a').each do |node|
       version = node.text.strip
 
       if version =~ /\A[0-9\.]+\z/
@@ -44,14 +26,14 @@ class Umbraco < Fingerprinter
   protected
 
   def download_link(url)
-    href = Nokogiri::HTML(Typhoeus.get(url).body).css('ul.projectGroups li div h3 a').attr('href')
+    href = Nokogiri::HTML(Typhoeus.get(url).body).css('div[class~="get-release"] div[class~="release"] h3 a').attr('href')
 
     "#{site_url}#{href}"
   end
 
   # The Extension is not in the url, so we force it
   # Otherwise the extraction will fail
-  def archive_extension(url)
+  def archive_extension(_url)
     '.zip'
   end
 end
