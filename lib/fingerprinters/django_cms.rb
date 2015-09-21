5eada528fdeface955d92fc6cbd0a9b285a5c3b1
erwanlr <erwan.lr@gmail.com>
2015-09-21 17:20:23 +0100

    Adds missing Django CMS fingerprints

--- lib/fingerprinters/django_cms.rb
+++ lib/fingerprinters/django_cms.rb
@@ -5,15 +5,27 @@ class DjangoCms < Fingerprinter
     versions = {}
     page     = Nokogiri::HTML(Typhoeus.get('https://github.com/divio/django-cms/releases').body)
 
-    page.css('h3 a span.tag-name').each do |node|
-      version = node.text.strip
+    loop do
+      page.css('h3 a span.tag-name').each do |node|
+        version = node.text.strip
 
-      next unless version =~ /\A[0-9\.]+\z/ # Only stable versions
+        next unless version =~ /\A[0-9\.]+\z/ # Only stable versions
 
-      versions[version] = "https://github.com/divio/django-cms/archive/#{version}.zip"
+        versions[version] = "https://github.com/divio/django-cms/archive/#{version}.zip"
+      end
+
+      page = next_page(page)
+      break unless page
     end
 
     versions
+  end
+
+  # @return [ Nokogiri::HTML, nil ] The next download page if any, or nil
+  def next_page(current_page)
+    link = current_page.search('div.pagination a:nth-child(2)').first
+
+    link ? Nokogiri::HTML(Typhoeus.get(link['href'].strip).body) : nil
   end
 
   def extract_archive(archive_path, dest)
