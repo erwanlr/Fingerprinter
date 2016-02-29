37467dfd026e9208225733f297d6c59fd5b2abc6
erwanlr <erwan.lr@gmail.com>
2016-02-29 13:57:12 +0000

    Fixes fckeditor downloadable_versions detection

--- lib/fingerprinters/fckeditor.rb
+++ lib/fingerprinters/fckeditor.rb
@@ -3,12 +3,12 @@
 class Fckeditor < Fingerprinter
   def downloadable_versions
     versions = {}
-    page     = Nokogiri::HTML(Typhoeus.get('http://sourceforge.net/projects/fckeditor/files/FCKeditor/').body)
+    page     = Nokogiri::HTML(Typhoeus.get('https://sourceforge.net/projects/fckeditor/files/FCKeditor/', cookie: 'FreedomCookie=true').body)
 
     page.css('a.name').each do |link|
       version = link.text.strip
       if version =~ /\A[0-9\.]+\z/ # Only Stables
-        versions[version] = "http://downloads.sourceforge.net/project/fckeditor/FCKeditor/#{version}/FCKeditor_#{version}.zip"
+        versions[version] = "https://downloads.sourceforge.net/project/fckeditor/FCKeditor/#{version}/FCKeditor_#{version}.zip"
       end
     end
 
