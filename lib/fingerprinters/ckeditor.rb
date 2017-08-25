b2d309858cc75e012f62809d54419299700e0b93
erwanlr <erwan.lr@gmail.com>
2017-08-25 19:11:06 +0100

    Ref #20 - Fix ckeditor updater

--- lib/fingerprinters/ckeditor.rb
+++ lib/fingerprinters/ckeditor.rb
@@ -3,7 +3,7 @@
 class Ckeditor < Fingerprinter
   def downloadable_versions
     versions = {}
-    page = Nokogiri::HTML(Typhoeus.get('http://ckeditor.com/download/releases').body)
+    page = Nokogiri::HTML(Typhoeus.get('https://ckeditor.com/download/releases').body)
 
     page.css('div p a').each do |link|
       version = link.attr('href').strip[/ckeditor_([0-9.]+)(?:_standard)?.zip$/, 1]
