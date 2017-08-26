cb2748340e344f7ba5765f734947033f59e4ec4a
erwanlr <erwan.lr@gmail.com>
2017-08-26 08:36:11 +0100

    Fix MediaElement updater

--- lib/fingerprinters/mediaelement.rb
+++ lib/fingerprinters/mediaelement.rb
@@ -6,7 +6,7 @@ class Mediaelement < Fingerprinter
 
   def downloadable_versions
     # versions below 1.0.4 are deleted as they do not contain the JS files
-    github_releases('johndyer/mediaelement').delete_if do |key, _|
+    github_releases('mediaelement/mediaelement').delete_if do |key, _|
       %w(1.0.3 1.0.2 1.0.1 1.0.0).include?(key)
     end
   end
