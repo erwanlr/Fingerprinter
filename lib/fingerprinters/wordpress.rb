6771279a05ae95a6adcd1e0933d01afdd56ac248
erwanlr <erwan.lr@gmail.com>
2018-08-14 19:43:45 +0100

    Fixes #33

--- lib/fingerprinters/wordpress.rb
+++ lib/fingerprinters/wordpress.rb
@@ -3,13 +3,10 @@
 class Wordpress < Fingerprinter
   def downloadable_versions
     versions = {}
-    page     = Nokogiri::HTML(Typhoeus.get('https://wordpress.org/download/releases/').body)
 
-    page.css('table.releases tr td:first').each do |node|
-      version = node.text.strip
+    json = JSON.parse(Typhoeus.get('https://api.wordpress.org/core/stable-check/1.0/').body)
 
-      next unless version =~ /\A[\d\.]+\z/
-
+    json.keys.each do |version|
       versions[version] = "http://wordpress.org/wordpress-#{version}.zip"
     end
 
