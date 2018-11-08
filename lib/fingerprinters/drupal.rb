85a29d0ce0e6bb5281e1b3d6f1ada931ea07026a
erwanlr <erwan.lr@gmail.com>
2018-11-08 18:06:00 +0000

    Updates FPs

--- lib/fingerprinters/drupal.rb
+++ lib/fingerprinters/drupal.rb
@@ -1,28 +1,21 @@
-
 # Drupal
 class Drupal < Fingerprinter
   include IgnorePattern::PHP
 
   def downloadable_versions
-    base_url = 'https://www.drupal.org/node/3060/release?&page=%i'
-    page_id  = 0
     versions = {}
+    page     = Nokogiri::HTML(Typhoeus.get(release_url).body)
 
-    loop do
-      page = Nokogiri::HTML(Typhoeus.get(format(base_url, page_id), headers: { 'User-Agent' => 'cURL' }).body)
+    page.xpath('//a[starts-with(@href, "drupal")]/@href').each do |node|
+      version = node.text.strip[/\Adrupal-([0-9.]+)\.tar\.gz\z/i, 1]
 
-      page.css('span.file a').each do |node|
-        version = node.text.strip[/\Adrupal-([0-9.]+).tar.gz\z/i, 1]
-
-        if version
-          versions[version] = "http://ftp.drupal.org/files/projects/#{node.text.strip}"
-        end
-      end
-
-      break if page.css('li.pager-next').empty?
-      page_id += 1
+      versions[version] = "#{release_url}#{node.text.strip}" if version
     end
 
     versions
+  end
+
+  def release_url
+    @release_url ||= 'https://ftp.drupal.org/files/projects/'
   end
 end
