3820a1d79827356dff85494c59ee9e8356640be2
erwanlr <erwan.lr@gmail.com>
2020-11-23 18:24:30 +0100

    Updates FPs

--- lib/fingerprinters/bolt.rb
+++ lib/fingerprinters/bolt.rb
@@ -6,12 +6,12 @@ class Bolt < Fingerprinter
   include IgnorePattern::PHP
 
   def downloadable_versions
-    github_releases('bolt/bolt', %r{/download/v?(?<v>[\d\.]+)/bolt\-v?[\d\.]+\.zip}i)
+    github_releases('bolt/bolt', %r{/download/v?(?<v>[\d.]+)/bolt-v?[\d.]+\.zip}i)
   end
 
   def extract_archive(archive_path, dest)
     super(archive_path, dest)
 
-    rebase(File.join(dest, 'public'), dest)
+    rebase(File.join(dest, 'public'), dest) if Pathname.new(dest).join('public').directory?
   end
 end
